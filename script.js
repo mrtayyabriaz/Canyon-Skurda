2fef8b62760a3f8062333b7248f7e126c7afc879
Tayyab Riaz <mrtayyabriaz@gmail.com>
2024-08-04 12:10:53 +0500

    nav bar fancy box

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleMobileMenu() {
+  const mobileMenu = document.getElementById("mobile-menu");
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.remove("hidden");
+  } else {
+    mobileMenu.classList.add("hidden");
+  }
+}
